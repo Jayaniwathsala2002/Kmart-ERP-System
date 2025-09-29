2bba04e43cc8e5fbaa3ff362c47637bf8ec0a3d4
Jayaniwathsala2002 <me.janu14@gmail.com>
2025-09-29 10:14:59 +0530

    ok

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -54,7 +54,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e8870767a388999c9bba02aa0e386f2e
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "2d54e3e7c0ef099a0c6ce47560f935df",
+"flutter_bootstrap.js": "f824c652ea56a53f4bc2723308b9b231",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
